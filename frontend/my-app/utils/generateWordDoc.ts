883702198e8e03d767cbb8a55480cb3094d0ab24
shadab-iqbal <shadabiqbalmu@gmail.com>
2026-06-17 18:23:52 -0400

    remove file-saver import

--- frontend/my-app/utils/generateWordDoc.ts
+++ frontend/my-app/utils/generateWordDoc.ts
@@ -1,5 +1,4 @@
 import { Document, Packer, Paragraph, TextRun, ExternalHyperlink } from "docx";
-import { saveAs } from "file-saver";
 import { FacilityData, ManualInputs } from "../types/facility";
 
 function row(label: string, value: string | null | undefined): Paragraph {
