8b43a3d6ebc8cbb03601c0e2615ed10f95e62dd3
shadab-iqbal <shadabiqbalmu@gmail.com>
2026-06-17 18:32:30 -0400

    add frontend link to cors

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,10 @@ app = FastAPI(title="Medelite Facility Assessment API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://facility-assessment-report-generato.vercel.app/",  # ← add this
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
